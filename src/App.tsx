7d76da80098b485286e12f33efb4fab45a6d64d9
Rafaelzera7 <rafael.fernandesAIMEC@gmail.com>
2022-10-13 17:24:46 -0300

    save

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/globals.css'
+export function App() {
+
+  return (
+    <div className="App">
+    <h1 className='font-bold tex-5xl text-cyan-500'>Hello World</h1>
+    </div>
+  )
+}
+
